0838cfb1cd3d5bedff1b7b737e2e73d135c91a30
Katarnik <kniewczas@inf.ug.edu.pl>
2016-04-04 16:40:09 +0200

    Added creating message by client.

--- lab5/klient.c
+++ lab5/klient.c
@@ -1,7 +1,62 @@
 #include <fcntl.h> 
-#include <unistd.h> 
+#include <unistd.h>
+#include <stdlib.h> 
 #include <stdio.h>
+#include <string.h>
+
+typedef struct _CLIENT_DATA
+{
+	int messageLength;
+	int ID;
+	char * homePath;
+}CLIENT_DATA;
+
+ int getHomePath(CLIENT_DATA *data)
+{
+	data->homePath = getenv("HOME");
+	
+	if(data->homePath != NULL)
+	{
+	   return 0;
+	}
+	return 1;
+}
 
 int main()
 {
+	//int result;
+	
+	CLIENT_DATA data;
+	CLIENT_DATA *data2 = &data;
+	CLIENT_DATA *data3 = &data; 
+	int length = 0;
+	unsigned char * message = malloc(sizeof(CLIENT_DATA));
+
+
+	data2->ID = 1;
+	data2->homePath = getenv("HOME");
+	length = sizeof(int) + strlen(data2->homePath);
+	data2->messageLength = length;
+	
+	memcpy(message, data2, sizeof(*data2));
+	printf("%c" , message[0]);
+	
+	//przekazac message przez kolejke fifo
+	//dopisac parser wiadomosci po stronie serwera
+	
+	//memcpy(data3, message, strlen(message));
+	
+	/*if(result == 1)
+	{
+		printf("Writing name failed");
+		return 1;
+	}
+	else
+	{
+		printf("%s", data2->homePath);	
+	}*/
+	
+	printf("%s" , data3->homePath);	
+		
+	return 0;
 }
--- lab5/serwer.c
+++ lab5/serwer.c
@@ -12,33 +12,31 @@ typedef struct _DATA_ENTRY
 
 void createData(DATA_ENTRY database[], int databaseSize)
 {
-	 FILE *fp;
-	 char str[MAX_SURNAME_LENGTH];
-	 int i = 0;
-	  
-	 fp = fopen("data.txt" , "r");
-	 
-	 if (fp != NULL)
-	 {
-		 while(fgets(str,MAX_SURNAME_LENGTH, fp) != NULL)
-		 {
-			//printf("%s" , str);
-			database[i].ID = i;
-			database[i].surname = "";
-			strcpy(database[i].surname, str);
-			i++;
-		 }
-	 }
-	 else
-	 {	
-		 printf("blad bazy danych");
-	 }
+	// TEST DATA
+	
+	database[0].ID = 1;
+	database[0].surname = "Katarzyna";
+	
+	database[1].ID = 2;
+	database[1].surname = "Pawel";
+	
+	database[2].ID = 3;
+	database[2].surname = "Maciek";
 }
 
 int main()
 {
+	//TO DO
+	
+	// [x] - database
+	// [] - create client message
+	// [] - parsing client message 
+	// [] - create fifo files
+	
 	DATA_ENTRY database[DATABASE_SIZE] ;
+	
 	createData(database, DATABASE_SIZE);
-	printf("%d", database[0].ID);
+	
+	
 	return 0;
 }
